51d6c0141d3af74e998cceb917c4d883d36ccf80
Tom ROUET <rouettom@gmail.com>
2021-03-05 18:56:18 +0100

    Add SPI enabler to setup

--- setup3.7.py
+++ setup3.7.py
@@ -8,6 +8,12 @@ system(f"sudo cp {path.join(path.dirname(__file__), 'src/Font.ttc')} {path.join(
 system(f"sudo cp {path.join(path.dirname(__file__), 'src/hardware.log')} {path.join(getenv('INSTALL_DIR'), 'hardware.log')}")
 system(f"sudo cp {path.join(path.dirname(__file__), 'src/hardware.py')} {path.join(getenv('INSTALL_DIR'), 'hardware.py')}")
 
+# Enable SPI
+rpi_config_content = "dtparam=spi=on"
+rpi_config_file = open("/boot/config.txt", "a")
+rpi_config_file.write(rpi_config_content)
+rpi_config_file.close()
+
 # Hardware as a service
 hardware_service_content = f"[Unit]\nDescription=Control of the pi by hardware\nAfter=network.target\n\n[Service]\nType=simple\nRestart=on-failure\nExecStart=/usr/bin/python3.7 {path.join(getenv('INSTALL_DIR'), 'hardware.py')}\n\n[Install]\nWantedBy=multi-user.target"
 hardware_service_file = open("/etc/systemd/system/hardware.service", "w")
